cfd04002e1695dbbc805cd63afa48fa036278f9b
Zlatko Alomerovic <zlatko@miavision.net>
2016-11-04 11:46:48 +0100

    Refactoring routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }     from './dashboard.component';
+import { HeroService }         from './hero.service';
+
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
+
 export class AppModule { }
